7f2ae5ff629715b4419940a3c0366a9e5d036a70
Aissaoui <saber96aissaoui@gmail.com>
2023-05-23 21:53:09 +0200

    optimising navbar style

--- src/Components/NavBar.js
+++ src/Components/NavBar.js
@@ -1,36 +1,25 @@
 import React from 'react'
 import './NavBar.css'
 import logo from '../Assets/images/logo_dark.svg'
-import search from '../Assets/images/icons/navBar/loupe.png'
-import notification from '../Assets/images/icons/navBar/bell.png'
-import messages from '../Assets/images/icons/navBar/messenger.png'
 import profile from '../Assets/images/profile.png'
 import { useNavigate } from "react-router-dom";
 
 function NavBar() {
     let navigate = useNavigate();
-    return <div className='navBarContainer'>
-        <div id='left-block'>
-            <img src={logo} alt='logo' className='logo' onClick={() =>navigate("/")}></img>
-            <label className='pages' onClick={() =>navigate("/Offres")} >Offres</label>
-            <label className='pages' onClick={()=>navigate("/Demandes")}>Demandes</label>
-        <label className='pages'>Aide</label>
+    return <div className='navbar'>
+        <img className='navbar__logo' src={logo} alt='logo' onClick={() => navigate("/")}></img>
+        <div className='navbar__pages'>
+            <label className='navbar__pages__page' onClick={() => navigate("/Offres")} >Offres</label>
+            <label className='navbar__pages__page' onClick={() => navigate("/Demandes")}>Demandes</label>
+            <label className='navbar__pages__page'>Aide</label>
         </div>
-        <div id='right-block'>
-            <input type='button' value='PUBLIER' className='greenButton' onClick={ ()=>navigate("/Publication") }></input>
-        <img src={search} alt='rechercher' id='search-icon'></img>
-        <div className='icon-container'>
-            <img src={notification} alt='notifications'></img>
-            <div><p>3</p></div>
-        </div>
-        <div className='icon-container'>
-            <img src={messages} alt='messages'></img>
-            <div><p>1</p></div>
-        </div>
-        <div id='account'>
-            <img src={profile} alt='messages' className='icon'></img>
-            <label>a3dsm2011</label>
-        </div>
+        <input className='navbar__publish button' type='button' value='PUBLIER' onClick={() => navigate("/Publication")}></input>
+        <div className='navbar__search'></div>
+        <div className='navbar__notifications'></div>
+        <div className='navbar__messages'></div>
+        <div className='navbar__account'>
+            <img className='navbar__account__image' src={profile} alt='messages'></img>
+            <label className='navbar__account__username'>a3dsm20110123456</label>
         </div>
     </div>
 }
